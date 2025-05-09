e59aa91dc372ea4fe13315e89782af6a3f4976e0
Layton <layton.ab10@gmail.com>
2025-05-09 14:23:08 -0400

    wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+
+
+local config = {}
+  config.color_scheme = 'Obsidian'
+  config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
+  
+  local mux = wezterm.mux
+  wezterm.on("gui-startup", function(cmd)
+      local tab, pane, window = mux.spawn_window(cmd or {})
+      window:gui_window():maximize()
+  end)
+
+return config
